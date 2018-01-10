2cde06035beedf96e1b2fd7f694fceccb5acb427
mybios <mybios@qq.com>
2018-01-11 07:46:12 +0800

    调整console输出

--- packages/pinus-logger/lib/logger.ts
+++ packages/pinus-logger/lib/logger.ts
@@ -111,7 +111,7 @@ declare global {
 }
 
 function replaceConsole() {
-    const logger = getLogger('console');
+    const logger = getLogger('logger' , 'console');
     console.debug = logger.debug.bind(logger);
     console.log = logger.info.bind(logger);
     console.warn = logger.warn.bind(logger);
