6246b3afdd21345c95422e8403bc88379a2084bd
mybios <mybios@qq.com>
2018-01-18 21:27:19 +0800

    空连接断开不打日志

--- packages/pinus/lib/connectors/hybrid/switcher.ts
+++ packages/pinus/lib/connectors/hybrid/switcher.ts
@@ -78,7 +78,6 @@ export class HybridSwitcher extends EventEmitter implements IHybridSwitcher {
         }
 
         socket.on('error',  (err: Error) => {
-            logger.warn('connection error:%s, the remote ip is %s && port is %s', err.message, socket.remoteAddress, socket.remotePort);
             socket.destroy();
         });
 
