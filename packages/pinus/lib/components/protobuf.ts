969464d5771fc5b021016e465e44675f6c5c92eb
smallwhite <whitehack@163.com>
2018-03-29 11:19:59 +0800

    pinus:修改protbuf。ts 来支持 require方式载入数据。

--- packages/pinus/lib/components/protobuf.ts
+++ packages/pinus/lib/components/protobuf.ts
@@ -31,6 +31,15 @@ export class ProtobufComponent implements IComponent {
     protobuf: Protobuf;
     name = '__protobuf__';
 
+    _canRequire(path:string):boolean{
+        try{
+            require.resolve(path);
+        }catch(err){
+            return false;
+        }
+        return true;
+    }
+
     constructor(app: Application, opts ?: ProtobufComponentOptions) {
         this.app = app;
         opts = opts || {};
@@ -41,8 +50,8 @@ export class ProtobufComponent implements IComponent {
         let originClientPath = path.join(app.getBase(), Constants.FILEPATH.CLIENT_PROTOS);
         let presentClientPath = path.join(Constants.FILEPATH.CONFIG_DIR, env, path.basename(Constants.FILEPATH.CLIENT_PROTOS));
 
-        this.serverProtosPath = opts.serverProtos || (fs.existsSync(originServerPath) ? Constants.FILEPATH.SERVER_PROTOS : presentServerPath);
-        this.clientProtosPath = opts.clientProtos || (fs.existsSync(originClientPath) ? Constants.FILEPATH.CLIENT_PROTOS : presentClientPath);
+        this.serverProtosPath = opts.serverProtos || (this._canRequire(originServerPath) ? Constants.FILEPATH.SERVER_PROTOS : presentServerPath);
+        this.clientProtosPath = opts.clientProtos || (this._canRequire(originClientPath) ? Constants.FILEPATH.CLIENT_PROTOS : presentClientPath);
 
         this.setProtos(Constants.RESERVED.SERVER, path.join(app.getBase(), this.serverProtosPath));
         this.setProtos(Constants.RESERVED.CLIENT, path.join(app.getBase(), this.clientProtosPath));
@@ -76,7 +85,7 @@ export class ProtobufComponent implements IComponent {
     }
 
     setProtos(type: string, path: string) {
-        if (!fs.existsSync(path)) {
+        if (!this._canRequire(path)) {
             return;
         }
 
@@ -92,7 +101,8 @@ export class ProtobufComponent implements IComponent {
         this.version = crypto.createHash('md5').update(protoStr).digest('base64');
 
         // Watch file
-        let watcher = fs.watch(path, this.onUpdate.bind(this, type, path));
+        const truePath = require.resolve(path);
+        let watcher = fs.watch(truePath, this.onUpdate.bind(this, type, truePath));
         if (this.watchers[type]) {
             this.watchers[type].close();
         }
@@ -105,25 +115,24 @@ export class ProtobufComponent implements IComponent {
         }
 
         let self = this;
-        fs.readFile(path, 'utf8',  (err, data) => {
-            try {
-                let protos = Protobuf.parse(JSON.parse(data));
-                if (type === Constants.RESERVED.SERVER) {
-                    this.protobuf.setEncoderProtos(protos);
-                    self.serverProtos = protos;
-                } else {
-                    this.protobuf.setDecoderProtos(protos);
-                    self.clientProtos = protos;
-                }
-
-                let protoStr = JSON.stringify(self.clientProtos) + JSON.stringify(self.serverProtos);
-                self.version = crypto.createHash('md5').update(protoStr).digest('base64');
-                logger.info('change proto file , type : %j, path : %j, version : %j', type, path, self.version);
-            } catch (e) {
-                logger.warn('change proto file error! path : %j', path);
-                logger.warn(e);
+        delete require.cache[path];
+        try {
+            let protos = Protobuf.parse(require(path));
+            if (type === Constants.RESERVED.SERVER) {
+                this.protobuf.setEncoderProtos(protos);
+                self.serverProtos = protos;
+            } else {
+                this.protobuf.setDecoderProtos(protos);
+                self.clientProtos = protos;
             }
-        });
+
+            let protoStr = JSON.stringify(self.clientProtos) + JSON.stringify(self.serverProtos);
+            self.version = crypto.createHash('md5').update(protoStr).digest('base64');
+            logger.info('change proto file , type : %j, path : %j, version : %j', type, path, self.version);
+        } catch (e) {
+            logger.warn('change proto file error! path : %j', path);
+            logger.warn(e);
+        }
     }
 
     stop(force: boolean, cb: () => void) {
