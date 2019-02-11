c4ee2ecf8176a89527a0fcbb5a916228245b3f53
smallwhite <whitehack@163.com>
2019-02-11 10:40:44 +0800

    添加生命周期(ILifeCycle 接口)示例。

---
+++ examples/websocket-chat/game-server/app/servers/connector/lifecycle.ts
@@ -0,0 +1,27 @@
+import { ILifeCycle, Application } from "pinus";
+
+export default function () {
+    return new Lifecycle();
+}
+
+
+class Lifecycle implements ILifeCycle {
+    beforeStartup(app: Application, cb: () => void): void {
+        console.log(app.getServerId(), '!!!before startup');
+        cb();
+    }
+
+    afterStartup(app: Application, cb: () => void): void {
+        console.log(app.getServerId(), '!!afterStartup');
+        cb();
+    }
+
+    afterStartAll(app: Application): void {
+        console.log(app.getServerId(), '!!after start all');
+    }
+
+    beforeShutdown(app: Application, shutDown: () => void, cancelShutDownTimer: () => void) {
+        console.log(app.getServerId(), '!!beforeShutdown');
+        shutDown();
+    }
+}
