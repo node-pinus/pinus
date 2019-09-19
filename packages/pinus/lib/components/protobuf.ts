3d20811bd1ad5a43bb9d89caac8b96651c2dadd6
smallwhite <whitehack@163.com>
2019-09-19 14:02:17 +0800

    [pinus]protobuf.ts 预防 git checkout这样的操作导致获得的数据为空的情况

--- packages/pinus/lib/components/protobuf.ts
+++ packages/pinus/lib/components/protobuf.ts
@@ -141,6 +141,11 @@ export class ProtobufComponent implements IComponent {
         this.clearRequireCache(path);
         try {
             let protos = Protobuf.parse(require(path));
+            // 预防 git checkout这样的操作导致获得的数据为空的情况
+            if (!protos || !Object.keys(protos).length) {
+                // retry.
+                throw new Error("protos error")
+            }
             if (type === Constants.RESERVED.SERVER) {
                 this.protobuf.setEncoderProtos(protos);
                 self.serverProtos = protos;
