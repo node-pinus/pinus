8d98c1c5504471eee9bcc45ac9bb93c6df811c9d
Wetion <wetiontop@gmail.com>
2020-03-11 20:09:42 +0800

    修复web-server兼容express 4.17.1 版本问题

--- packages/pinus/template/ts/web-server/app.js
+++ packages/pinus/template/ts/web-server/app.js
@@ -1,25 +1,28 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorHandler = require('errorhandler')
+const methodOverride = require('method-override');
 
-app.use(express.methodOverride());
-app.use(express.bodyParser());
-app.use(app.router);
+var app = express();
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/public');
 app.set('view options', {layout: false});
 app.set('basepath',__dirname + '/public');
 
 let env = app.get('env');
-if(env=="development"){
+if (env == 'development') {
   app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-}
-if(env == "production"){
-  var oneYear = 31557600000;
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }));
 }
 
-console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
+if (env == 'production') {
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(errorHandler());
+}
+
+console.log('Web server has started.\nPlease log on http://127.0.0.1:3001/index.html');
 
 app.listen(3001);
