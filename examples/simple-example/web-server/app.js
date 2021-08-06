ba014f52852e787fd6964ec8b1099bdefd5f2e88
smallwhite <whitehack@163.com>
2021-08-06 14:20:23 +0800

    template: 修复 web-server 启动报错问题

--- examples/simple-example/web-server/app.js
+++ examples/simple-example/web-server/app.js
@@ -11,12 +11,10 @@ var app = express();
 let env = app.get('env');
 if(env=="development"){
   app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 }
 if(env == "production"){
   var oneYear = 31557600000;
   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
 }
 
 console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
