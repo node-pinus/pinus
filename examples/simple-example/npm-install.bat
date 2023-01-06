1b8d92b642f44526b3196edb0965ee5746bcc9a4
smallwhite <whitehack@163.com>
2023-01-06 14:04:28 +0800

    ignore simple-example/npm-install.bat change

--- examples/simple-example/npm-install.bat
+++ examples/simple-example/npm-install.bat
@@ -1,4 +1,4 @@
 ::npm-install.bat
 @echo off
 ::install web server dependencies && game server dependencies
-cd web-server && yarn && cd .. && cd game-server && yarn
+cd web-server && npm install -d && cd .. && cd game-server && npm install -d
