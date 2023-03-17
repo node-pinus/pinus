49fc88198d6e78297274f79bc560ef19aa20310a
gzhCompany <981352165@qq.com>
2023-03-17 15:04:51 +0800

    修复mqtt的rpc-client概率自动断开连接问题

--- packages/pinus-rpc/lib/rpc-client/mailboxes/mqtt-mailbox.ts
+++ packages/pinus-rpc/lib/rpc-client/mailboxes/mqtt-mailbox.ts
@@ -216,18 +216,10 @@ export class MailBox extends EventEmitter implements IMailBox {
         // console.log('checkKeepAlive lastPing %d lastPong %d ~~~', this.lastPing, this.lastPong);
         let now = Date.now();
         let KEEP_ALIVE_TIMEOUT = this.keepalive * 2;
-        if (this.lastPing > 0) {
-            if (this.lastPong < this.lastPing) {
-                if (now - this.lastPing > KEEP_ALIVE_TIMEOUT) {
-                    logger.error('mqtt rpc client %s checkKeepAlive timeout from remote server %s for %d lastPing: %s lastPong: %s', this.serverId, this.id, KEEP_ALIVE_TIMEOUT, this.lastPing, this.lastPong);
-                    this.emit('close', this.id);
-                    this.lastPing = -1;
-                    // this.close();
-                }
-            } else {
-                this.socket.pingreq();
-                this.lastPing = Date.now();
-            }
+        if (this.lastPong < this.lastPing && now - this.lastPing > KEEP_ALIVE_TIMEOUT) {
+            logger.error('mqtt rpc client %s checkKeepAlive timeout from remote server %s for %d lastPing: %s lastPong: %s', this.serverId, this.id, KEEP_ALIVE_TIMEOUT, this.lastPing, this.lastPong);
+            this.emit('close', this.id);
+            this.lastPing = -1;
         } else {
             this.socket.pingreq();
             this.lastPing = Date.now();
